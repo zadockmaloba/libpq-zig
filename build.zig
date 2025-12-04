010f895bd503b82b055e0ff70126f0cbe659223d
Antoine GAGNIERE <antoine.gagniere@datadoghq.com>
2025-12-04 14:51:28 +0100

    Use new module from allyoucodebase/libpq that contains bindings and linked library

--- build.zig
+++ build.zig
@@ -1,46 +1,34 @@
 const std = @import("std");
 
-
 pub fn build(b: *std.Build) void {
-
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const postgres = b.dependency("libpq", .{
         .target = target,
         .optimize = optimize,
-        //.@"disable-ssl" = true,
     });
-
     const zeit = b.dependency("zeit", .{
         .target = target,
         .optimize = optimize,
     });
 
-    const libpq = postgres.artifact("pq");
-    b.installArtifact(libpq);
-
     const mod = b.addModule("libpq_zig", .{
-
         .root_source_file = b.path("src/root.zig"),
-
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "libpq", .module = postgres.module("libpq") },
+        },
     });
-
     const datetime_mod = b.addModule("datetime", .{
         .root_source_file = b.path("src/datetime.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "zeit", .module = zeit.module("zeit") },
+        },
     });
-
-    mod.addImport("zeit", zeit.module("zeit"));
-    datetime_mod.addImport("zeit", zeit.module("zeit"));
-
-    mod.addIncludePath(b.path("zig-out/include/"));
-    mod.addIncludePath(b.path("zig-out/include/libpq/"));
-    mod.linkLibrary(libpq);
 
     const mod_tests = b.addTest(.{
         .root_module = mod,
